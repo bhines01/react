dd25151430e5ea8a797b27c0f32a145ed567e846
Bryan <bryan.hines@villanova.edu>
2022-10-18 22:14:04 -0400

    removed outlet

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from 'react-router-bootstrap';
-import {Outlet} from 'react-router-dom';
 
 
 function Header() {
