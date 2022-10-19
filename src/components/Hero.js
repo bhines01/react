fe09f5567ef0758ff2273e5f56e9c0ba3ed6d913
Bryan <bryan.hines@villanova.edu>
2022-10-18 22:19:52 -0400

    updated hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,7 @@ function Hero() {
                 <Image src={heroImage} className='d-block mx-lg-auto img-fluid'/>
             </Col>
             <Col lg={6}>
-                <h1 className='display-5 fw-bold lh-1 mb-3'>Welcome, take a look around</h1>
+                <h1 className='display-5 fw-bold lh-1 mb-3'>Welcome, take a look around and have fun!</h1>
                 <p className='lead'>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
